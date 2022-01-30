886790393e8d983ae4bc27879999b60928394568
TheUnnamedPerson <40750101+TheUnnamedPerson@users.noreply.github.com>
2022-01-29 21:06:47 -0800

    Inverse Kinematics 3/? + Visual Studio Solution

---
+++ HeistBot/MovementEngine.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Vector2.h"
+class MovementEngine
+{
+public:
+    float UpperLength = 1;
+    float LowerLength = 1;
+
+    float pastMotorAngles[2];
+
+    Vector2 MotorAngles(Vector2 newPosition);
+};
+
