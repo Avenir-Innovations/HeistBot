efe44314c034d6f5ed7930f62ffebb2a6f2bfd85
TheUnnamedPerson <40750101+TheUnnamedPerson@users.noreply.github.com>
2022-01-30 19:57:28 -0800

    Ported to Linux 3/3

---
+++ HeistBot/Vector2.cpp
@@ -0,0 +1,19 @@
+#include "Vector2.h"
+
+Vector2::Vector2()
+{
+    x = 0;
+    y = 0;
+}
+
+Vector2::Vector2(float _x, float _y)
+{
+        x = _x;
+        y = _y;
+}
+
+Vector2::Vector2(float _f[2])
+{
+    x = _f[0];
+    y = _f[1];
+}
