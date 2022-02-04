aa35b46f74feb8e9f63107bfe4dc8b80affcbf36
Fernando Fonseca <40750101+TheUnnamedPerson@users.noreply.github.com>
2022-02-03 16:58:46 -0800

    Shapes 2/?

--- HeistBot/shape.cpp
+++ HeistBot/shape.cpp
@@ -5,11 +5,44 @@ using namespace std;
 
 bool shape::IsInside(Vector2 pos)
 {
-	bool _in = false;
+	bool _in = true;
+	bool left = false;
+	bool right = false;
 	
-
-	for (int i = 0; i < ; i++)
+	if (pos.x <= vertices[0].x || pos.x <= vertices[1].x)
 	{
-
+		if (!((pos.y >= vertices[0].y && pos.y <= vertices[1].y) || (pos.y <= vertices[0].y && pos.y >= vertices[1].y))) _in = false;
+		else left = true;
 	}
+	else if (pos.x >= vertices[0].x || pos.x >= vertices[1].x)
+	{
+		if (!((pos.y >= vertices[0].y && pos.y <= vertices[1].y) || (pos.y <= vertices[0].y && pos.y >= vertices[1].y))) _in = false;
+		else right = true;
+	}
+
+	if ((pos.x <= vertices[1].x || pos.x <= vertices[2].x) && !left)
+	{
+		if (!((pos.y >= vertices[1].y && pos.y <= vertices[2].y) || (pos.y <= vertices[1].y && pos.y >= vertices[2].y))) _in = false;
+		else left = true;
+	}
+	else if ((pos.x >= vertices[1].x || pos.x >= vertices[2].x) && !right)
+	{
+		if (!((pos.y >= vertices[1].y && pos.y <= vertices[2].y) || (pos.y <= vertices[1].y && pos.y >= vertices[2].y))) _in = false;
+		else right = true;
+	}
+
+	if ((pos.x <= vertices[2].x || pos.x <= vertices[0].x) && !left)
+	{
+		if (!((pos.y >= vertices[2].y && pos.y <= vertices[0].y) || (pos.y <= vertices[2].y && pos.y >= vertices[0].y))) _in = false;
+		else left = true;
+	}
+	else if ((pos.x >= vertices[2].x || pos.x >= vertices[0].x) && !right)
+	{
+		if (!((pos.y >= vertices[2].y && pos.y <= vertices[0].y) || (pos.y <= vertices[2].y && pos.y >= vertices[0].y))) _in = false;
+		else right = true;
+	}
+
+	if (!(right && left)) _in = false;
+
+	return _in;
 }
