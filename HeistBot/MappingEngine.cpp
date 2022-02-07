c1af026b7aab996db4c34136b7cf49f88acd1d04
Fernando Fonseca <40750101+TheUnnamedPerson@users.noreply.github.com>
2022-02-07 11:54:43 -0800

    Mapping 2/?

--- HeistBot/MappingEngine.cpp
+++ HeistBot/MappingEngine.cpp
@@ -1,1 +1,55 @@
 #include "MappingEngine.h"
+
+void MappingEngine::GenerateShape (Vector2 _input)
+{
+    list<Shape>::iterator polygon = polygons.begin();
+    for (int i = 0; i < size(polygons); i++)
+    {
+        for (int i = 0; i < (*polygon).vertices.size(); i++)
+        {
+            
+        }
+    }
+}
+
+void MappingEngine::CheckShapes ()
+{
+    int inputSize = size(inputPoints);
+    list<Vector2> _ip = inputPoints;
+    list<Vector2>::iterator vertex = inputPoints.begin();
+    for (int ii = 0; ii < inputSize; ii++)
+    {
+        list<Shape>::iterator polygon = polygons.begin();
+        for (int i = 0; i < size(polygons); i++)
+        {
+            if ((find(((*polygon).vertices).begin(), ((*polygon).vertices).end(), ((*polygon).vertices)) != ((*polygon).vertices).end()) || (*polygon).IsInside(*vertex))
+            {
+                _ip.remove(*vertex);
+            }
+            else
+            {
+                GenerateShape(*vertex);
+                _ip.remove(*vertex);
+            }
+            if (i < size(polygons) - 1) polygon = next(polygon, 1);
+        }
+        if (ii < inputSize - 1) vertex = next(vertex, 1);
+    }
+}
+
+void MappingEngine::Update ()
+{
+    while (true)
+    {
+        std::thread thread1(CheckShapes);
+        
+        
+        thread1.join();
+    }
+}
+
+void MappingEngine::Initialize ()
+{
+    Update();
+}
+
--- HeistBot/MappingEngine.h
+++ HeistBot/MappingEngine.h
@@ -3,19 +3,27 @@
 #include "Shape.h"
 #include <array>
 #include <list>
-#include<thread>
-//std::thread thread_object(callable)
+#include <thread>
+#include <algorithm>
 
 class MappingEngine
 {
+	//(x,y) = x and y inches
+private:
+	list<Shape> polygons;
+	list<Shape> gernatedPolygons;
+
+	void GenerateShape (Vector2 _input);
+	void CheckShapes ();
+	void Update ();
+	
 public:
 	Vector2 Position;
 	list<Vector2> inputPoints;
 
 	// look for nearest shape(s), check if inside of nearest shape(s), if not create new polygon using the new point.
 	//TODO: Brainstorm How to extend new created shapes.
-private:
-	list<Shape> polygons;
-	
+
+	void Initialize ();	
 };
 
