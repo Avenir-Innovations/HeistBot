f05c8e46dbe19dc4abe99803eb9499379316b0c2
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-04-18 11:54:00 -0700

    Create VisionEngine.cpp

---
+++ HeistBot/Vision/VisionEngine.cpp
@@ -0,0 +1,12 @@
+#include "VisionEngine.h"
+
+using namespace HeistBot;
+using namespace Vision;
+using namespace cv;
+
+Vector2 VisionEngine::ReturnCoords(int x1, int y1, int x2, int y2)
+{
+  list<Vector2> coords;
+
+  return coords;
+}
