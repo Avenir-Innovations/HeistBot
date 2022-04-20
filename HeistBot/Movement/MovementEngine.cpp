c9d06392ed20281f655dd55945c35d3384df4f32
Fernando Fonseca <40750101+TheUnnamedPerson@users.noreply.github.com>
2022-04-20 11:49:01 -0700

    moveLeg()

--- HeistBot/Movement/MovementEngine.cpp
+++ HeistBot/Movement/MovementEngine.cpp
@@ -2,7 +2,7 @@
 
 using namespace HeistBot;
 using namespace Movement;
-using namespace Extras;
+using namespace CSS;
 
 Vector2 MovementEngine::MotorAngles(Vector2 newPosition)
 {
@@ -18,7 +18,53 @@ Vector2 MovementEngine::MotorAngles(Vector2 newPosition)
     return Vector2({thetaA, thetaB});
 }
 
-void MovementEngine::moveLeg(Vector2 pos, float speed)
+void MovementEngine::moveLeg(int legID, Vector2 pos, float speed)
 {
-    
+    Vector2 Motors = Vector2();
+
+    switch (legID)
+    {
+        case 1:
+            Motors = Vector2(1,2);
+            break;
+        case 2:
+            Motors = Vector2(3,4);
+            break;
+        case 3:
+            Motors = Vector2(5,6);
+            break;
+        case 4:
+            Motors = Vector2(7,8);
+            break;
+    }
+
+    Vector2 Thetas = MotorAngles(pos);
+
+    //Unsure If This Moves Just One Motor at a Time or if it Will Execute Both at Once.
+    RotateMotorPos(Motors.x, Thetas.x, speed);
+    RotateMotorPos(Motors.y, Thetas.y, speed);
+
+}
+
+void WalkForward(int Time)
+{
+    float Stride = PI / 4;
+    for (int i = 0; i < Time; i++)
+    {
+        //moveLeg();
+    }
+}
+
+void MovementEngine::RotateMotorPos(int motorID, float angle, float speed)
+{
+    //string dev_name("/dev/tty.usbmodemBE6118CD1");
+    int moteus_id = motorID;
+    //MoteusAPI api(dev_name, moteus_id); //For Use with the MoteusAPI
+
+    // send one position with speed and torque limits
+    double stop_position = RadToRot(angle);
+    double velocity = speed;
+    double max_torque = 1; //Example Value
+    double feedforward_torque = 0; //Example Value
+    //api.SendPositionCommand(stop_position, velocity, max_torque, feedforward_torque); //For Use with the MoteusAPI
 }
--- HeistBot/Movement/MovementEngine.h
+++ HeistBot/Movement/MovementEngine.h
@@ -17,16 +17,35 @@ namespace HeistBot
         class MovementEngine
         {
             public:
-                float UpperLength = 6;
-                float LowerLength = 10;
-                float pastMotorAngles[2];
+                float UpperLength = 6;  //Length of Upper Segments
+                float LowerLength = 10; //Length of Lower Segments
 
-                Vector2 MotorAngles(Vector2 newPosition);
+                Vector2 pastMotorAngles[4]; //See IDS for Order, Vector2s are (Theta A, Theta B)
+                
+                /*
+                    Ids for Motors and Legs are as Follows:
+                        Front Left  =  1   ;   A = 1   ;   B = 2
+                        Front Right =  2   ;   A = 3   ;   B = 4
+                        Back Left   =  3   ;   A = 5   ;   B = 6
+                        Back Right  =  4   ;   A = 7   ;   B = 8
+                        Arm         =  0   ;   
+                */
 
-                void RotateMotorPos(int motorID, float angle, float speed);
-                void RotateMotorVel(int motorID, float velocity, float time);
+                //  KEEP ALL ANGLES IN RADIANS  //
+
+                Vector2 MotorAngles(Vector2 newPosition); //Returns (Theta A, Theta B)
+
+                float RadToRot (float _rad) { return _rad / ( 2 * PI ); }       // Radians to Rotations
+                float RotToRad (float _rot) { return _rot * 2 * PI; }           // Rotations to Radians
+
+                //  Motors Use Rotations. Use the Methods Above For Conversion. //
+
+                void RotateMotorPos(int motorID, float angle, float speed);     //Position Based Motor Movement
+                void RotateMotorVel(int motorID, float velocity, float time);   //Velocity Based Motor Movement
 
                 void moveLeg(int legID, Vector2 pos, float speed);
+
+                void WalkForward(int Time);
         };
     }
 }
