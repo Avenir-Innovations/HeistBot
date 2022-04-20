bba713517a59485442482192b4a964bdd47cb35f
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-04-20 10:38:50 -0700

    Update VisionEngine.cpp

--- HeistBot/Vision/VisionEngine.cpp
+++ HeistBot/Vision/VisionEngine.cpp
@@ -6,7 +6,6 @@ using namespace cv;
 
 Vector2 VisionEngine::ReturnCoords(int x1, int y1, int x2, int y2)
 {
-  list<Vector2> coords;
-
+  list<Vector2> coords = {Vector2(x1, x2), Vector2(y1, y2)};
   return coords;
 }
